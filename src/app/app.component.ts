e6d579bb1a1da73736cc8ca72f97db97a843f734
rmj2626 <rudraksha.jejurikar21@pccoe.org>
2024-03-23 10:10:17 +0530

    abc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactUsPageComponent } from './contact-us-page/contact-us-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContactUsPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
